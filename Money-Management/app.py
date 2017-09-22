0fb597619a9ab311086f597e6e6445a6429dfa59
WillWu88 <williamwu8-8@outlook.com>
2017-09-22 12:49:58 -0400

    New Changes
    Working on writing the file

--- Money-Management/app.py
+++ Money-Management/app.py
@@ -1,31 +1,44 @@
 #reads and writes a file to keep record of my money
 
 #opens a hidden file names .checkbook.txt, if it doesnt exist, create a new one
-filename = ".checkbook.txt"
-try:
-    checkbook = open(filename, "r")
-except:
-    checkbook = open(filename, "w+")
-
+#filename = ".checkbook.txt"
+import os
 
 class User:
     #constructor
-    def __init__(self, monthly_allowance, number_of_months, name):
+    def __init__(self, monthly_allowance, number_of_months, name, filename):
+
         self.monthly_allowance = monthly_allowance
         self.number_of_months = number_of_months
-        self.name = name
+        self.name = input("What's your name?> ")
+        self.filename = "."+self.name + "ledger.txt"
+        try:
+            ledger = open(self.filename, "r")
+            ledger.close()
+        except:
+            ledger = open(self.filename, "w+")
+            ledger.close()
 
-    def printVar(var):
+    def printVar(self, var):
         '''debug func
         '''
         print(self.var)
-    def addOne():
+    def addOne(self):
         '''takes no arguments
         add one to the number of month
         '''
         number_of_months += 1
-    def printInfo():
+    def printInfo(self):
         '''prints info at every log in'''
+
+    def logIN(self):
+        '''writes register info everytime when the user log in
+        '''
+        if (os.stat(self.filename).st_size == 0):
+            with open(self.filename, "w") as ledger:
+                ledger.write("1")
+        else:
+
 
 
 #testing
