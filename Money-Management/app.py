3ec294e11294e1946505cfc4e0f2dbb8c8724298
WillWu88 <williamwu8-8@outlook.com>
2017-09-24 16:01:24 -0400

    Added new functions

--- Money-Management/app.py
+++ Money-Management/app.py
@@ -6,11 +6,11 @@ import os
 
 class User:
     #constructor
-    def __init__(self, monthly_allowance, number_of_months, name, filename):
+    def __init__(self, monthly_allowance, number_of_months, name):
 
         self.monthly_allowance = monthly_allowance
         self.number_of_months = number_of_months
-        self.name = input("What's your name?> ")
+        self.name = input("Log in: > ")
         self.filename = "."+self.name + "ledger.txt"
         try:
             ledger = open(self.filename, "r")
@@ -18,6 +18,8 @@ class User:
         except:
             ledger = open(self.filename, "w+")
             ledger.close()
+        #with open(self.filename, "w") as ledger:
+        #    ledger.write("0")
 
     def printVar(self, var):
         '''debug func
@@ -31,17 +33,28 @@ class User:
     def printInfo(self):
         '''prints info at every log in'''
 
+    def rewriteLine(self, line_num, replace_Str):
+        '''replace a line
+        going to be an intrinsic founction
+        so am not gonna put in user prompts
+        '''
+        with open(self.filename, "w") as ledger:
+            counter = 0
+            for lines in ledger:
+                if (counter-1 == line_num):
+                    ledger.write(line.replace(lines, replace_Str))
+
     def logIN(self):
         '''writes register info everytime when the user log in
         '''
         if (os.stat(self.filename).st_size == 0):
             with open(self.filename, "w") as ledger:
                 ledger.write("1")
-        else:
 
 
 
 #testing
 
 willWu = User(0, 0, "Hi")
-willWu.addOne
+willWu.logIN()
+'''willWu.rewriteLine(1,"1")'''
