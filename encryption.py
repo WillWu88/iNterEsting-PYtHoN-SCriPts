d1f0b55b25cc1d0b779ed5a1482f2fc7961851e0
WillWu88 <williamwu8-8@outlook.com>
2017-09-24 16:01:56 -0400

    finished encryption.py

--- encryption.py
+++ encryption.py
@@ -8,16 +8,30 @@ message = file.readline()
 print ("the unencrypted version is:"+message)
 
 #encrytion begins
+# temp arry holds message
 str_array = []
 for letters in message:
     str_array.insert(len(str_array),letters)
-print ("The array:"+str(str_array))
+#print ("The array:"+str(str_array))
+
+#holds encrypted message
+encrypted_array = []
 
 #declaring temp variables to be used later
-decryptKey = [] #holds the decryption keys, the difference between the actuall value and the real value
-a = 0
+decryptKey = []
+'''holds the decryption keys, the difference between the actuall
+value and the real value'''
 
-for letters in str_array:
-    shift = random.randrange(127)
-    print (shift)
-    break
+for chars in str_array:
+    a = random.randint(0,127)
+    encrypted_array.insert(len(encrypted_array), chr(a))
+    b = ord(chars) - a
+    decryptKey.insert(len(decryptKey),b)
+
+with open("encrypted.txt", "w+") as output_file:
+    for chars in encrypted_array:
+        output_file.write(chars)
+
+with open("decryptionKey.txt", "w+") as output_file:
+    for num in decryptKey:
+        output_file.write(str(num)+", ")
