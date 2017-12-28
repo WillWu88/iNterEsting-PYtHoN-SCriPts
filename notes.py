357a8f678426d81d3341b3e04fbaf6e155fe6a6f
Will Wu <williamwu8-8@outlook.com>
2017-12-28 13:52:18 +0800

    Added new notes

--- notes.py
+++ notes.py
@@ -1,9 +1,11 @@
-#some example codes and notes for review
+# some example codes and notes for review
+
 
 def ask_ok(prompt, retries=4, reminder='Please try again!'):
 while True:
+        # pattern matching in an array if....in
     ok = input(prompt)
-    if ok in ('y', 'ye', 'yes'): #pattern matching in an array if....in
+    if ok in ('y', 'ye', 'yes'):
         return True
     if ok in ('n', 'no', 'nop', 'nope'):
         return False
@@ -15,3 +17,24 @@ while True:
 #######################################################
 
 
+# French Desk Example
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+deck = FrenchDeck()  # constructor
+
+print(deck[0])
