18a61538b0a21e03b19bdcc3d4fa235e0bd2e432
WillWu88 <williamwu8-8@outlook.com>
2018-08-05 00:34:13 +0800

    a collection of encryption methods

---
+++ encrypt_new.py
@@ -0,0 +1,17 @@
+#file name is default: messsage.txt
+
+try:
+    file_name = 'message.txt'
+    file = open(file_name, "r")
+    message = file.readline()
+except:
+    message = ''
+    print('no file found')
+
+encrypted = []
+
+def constant_shift(shift):
+    for char in message:
+        encrypted.append(chr(ord(char) + shift))
+
+def random_shift
